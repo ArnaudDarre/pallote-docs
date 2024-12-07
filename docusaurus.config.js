b6986d7462b65db52375a897a7f6b9e2c1933d05
ArnaudDarre <arnaudarre@gmail.com>
2024-12-07 15:49:18 +0000

    Update site title and icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Pallote React',
+  tagline: 'Open-source React Component library',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -77,9 +77,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Pallote React',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Logo for Pallote React',
           src: 'img/logo.svg',
         },
         items: [
