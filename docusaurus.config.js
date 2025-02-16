ea87d212ff9efddab98a2db135ff905f58add95a
ArnaudDarre <arnaudarre@gmail.com>
2025-02-16 11:30:08 +0000

    Init hompage design

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,26 +11,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Pallote',
-  tagline: '',
+  tagline: 'Pallote is an handcrafted, simple and stylish CSS and React component library, built with passion and care',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://pallote.com/',
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ArnaudDarre',
+  projectName: 'pallote-docs',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -43,10 +36,6 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-            // 'https://github.com/ArnaudDarre/pallote-docs/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -54,11 +43,6 @@ const config = {
         //     type: ['rss', 'atom'],
         //     xslt: true,
         //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   // editUrl:
-        //     // 'https://github.com/ArnaudDarre/pallote-docs/',
-        //   // Useful options to enforce blogging best practices
         //   onInlineTags: 'warn',
         //   onInlineAuthors: 'warn',
         //   onUntruncatedBlogPosts: 'warn',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,111 @@
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import {
+  Buttons,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  Section,
+  SectionHeader,
+  SectionContent
+} from 'pallote-react';
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import InstallReact from '../components/index/InstallReact.mdx'
+import InstallCSS from '../components/index/InstallCSS.mdx'
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/installation">
-            Docusaurus Documentation
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+const InstallList = [
+  {
+    title: 'Pallote React',
+    subtitle: "Leverage the power of the CSS library included in simple, easy to use React components.",
+    markdownComponent: <InstallReact />,
+    linkURL: "/docs/getting-started/installation?package=react"
+  },
+  {
+    title: 'Pallote CSS',
+    subtitle: "Stylish CSS library for all your web projects.",
+    markdownComponent: <InstallCSS />,
+    linkURL: "/docs/getting-started/installation?package=css"
+  }
+]
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={siteConfig.title}
+      description={siteConfig.tagline}
+    >
       <main>
+        <Section fill='primary' landing>
+          <SectionHeader
+            title="Introducing Pallote"
+            subtitle={
+              <>
+                Pallote is a handcrafted, simple and stylish 
+                <Link className="ph-¼" to="/docs/getting-started/installation?package=react">React</Link>
+                and 
+                <Link className="ph-¼" to="/docs/getting-started/installation?package=css">CSS</Link>
+                component library, built with passion and care
+              </>
+            }
+            actions={
+              <Button
+                component={Link}
+                color="main"
+                size="lg"
+                to="/docs/getting-started/installation"
+              >
+                Get started
+              </Button>
+            }
+          />
+        </Section>
+        <Section id="section-install">
+          <SectionHeader
+            title="Install the packages"
+            subtitle="Get started by installing the packages in your project."
+          />
+          <SectionContent>
+            <Grid wrapper spacing={3} alignItems="stretch">
+              {InstallList.map((props, idx) => (
+                <Grid sm={4} key={idx} item>
+                  <Card hasShadow className="h-full">
+                    <CardHeader
+                      title={props.title}
+                      subtitle={props.subtitle}
+                    />
+                    <CardContent className="mt-auto">
+                      {props.markdownComponent}
+                    </CardContent>
+                    <CardActions className="mt-0">
+                      <Button fullWidth component={Link} to={props.linkURL}>Read the full doc</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </SectionContent>
+        </Section>
+        <Section fill="paper">
+          <SectionHeader
+            title="Contribute and support"
+            subtitle="Pallote is a passion project born of a love for coding and design. All contributions and feedback are welcome to help the project grow."
+            actions={
+              <Buttons>
+                <Button color="secondary" component="a" external href="https://github.com/sponsors/ArnaudDarre">
+                  Sponsor on GitHub
+                </Button>
+                {/* <Button component={Link} to="/contact">
+                  Give feedback
+                </Button> */}
+              </Buttons>
+            }
+          />
+        </Section>
       </main>
     </Layout>
   );
